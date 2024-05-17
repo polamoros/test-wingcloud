cdef7ee37b556883bc01fa5f1eff5a272e48b1e4
polamoros <info@polamoros.com>
2024-05-17 11:17:05 +0200

    wip

--- backend/target/test/main.wsim/.wing/preflight.commons-7.cjs
+++
@@ -1,9 +0,0 @@
-"use strict";
-const $stdlib = require('@winglang/sdk');
-const std = $stdlib.std;
-const $helpers = $stdlib.helpers;
-const $extern = $helpers.createExternRequire(__dirname);
-module.exports = {
-  ...require("./preflight.api-6.cjs"),
-};
-//# sourceMappingURL=preflight.commons-7.cjs.map
